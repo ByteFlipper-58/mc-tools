ed4a6d6ef108033ec604ff0e27af880118cc2bbc
Ibragim Maltsagov <byteflipper.business@gmail.com>
2024-07-21 20:22:02 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <div class="container mx-auto text-center">
+      <h1 class="text-3xl font-bold">Hello World!</h1>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Layout from './layouts/Layout.vue';
+
+export default {
+  components: {
+    Layout,
+  },
+};
+</script>
+
+<style scoped>
+/* Add any additional custom styles here */
+</style>
---
+++ src/layouts/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+      <!-- Header -->
+      <header class="bg-gray-800 text-white p-4">
+        <h1 class="text-2xl font-bold">MC Tools</h1>
+      </header>
+  
+      <!-- Main Content -->
+      <main class="flex-grow flex justify-center items-center p-4">
+        <slot></slot>
+      </main>
+  
+      <!-- Footer -->
+      <footer class="bg-gray-800 text-white p-4 text-center font-bold">
+        &copy; 2024 MC Tools by ByteFlipper
+      </footer>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Layout',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any additional custom styles here */
+  </style>
