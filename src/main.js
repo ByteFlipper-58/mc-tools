507124a8db1a9724d8e4ceced62813f66b63fac4
byteflipper.business <byteflipper.business@gmail.com>
2024-07-25 05:24:28 +0000

    Added Firebase Analytics

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import VueTelegram from 'vue-tg'
+import VueTelegram from 'vue-tg';
+import { analytics } from './firebase';
 import '@/assets/main.css';
 
-createApp(App)
-  .use(VueTelegram)
-  .use(router)
-  .mount('#app');
+const app = createApp(App);
+
+app.config.globalProperties.$analytics = analytics;
+
+app.use(VueTelegram);
+app.use(router);
+
+app.mount('#app');
